21b2aca5af2dbf0294bb8bf29adf2b509042ff80
MisterEggnog <12803156+MisterEggnog@users.noreply.github.com>
2021-04-18 15:36:40 -0700

    Prints nearest shelter

--- src/fireapi/__main__.py
+++ src/fireapi/__main__.py
@@ -9,6 +9,8 @@ zipcode = sys.argv[1]
 print(fireapi.commands.zipToLatLong(zipcode))
 print(fireapi.commands.zipToState(zipcode))
 
+fireapi.commands.nearestShelter(zipcode)
+
 #import catbot
 #import catbot.commands
 #import catbot.config
